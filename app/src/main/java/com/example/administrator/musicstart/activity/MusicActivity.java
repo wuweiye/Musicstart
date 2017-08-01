a6df93c117dd5af8891c32cbc0f2177bba798702
dkm <974685276@qq.com>
2017-08-01 13:28:01 +0800

    音乐下拉框修改

--- app/src/main/java/com/example/administrator/musicstart/activity/MusicActivity.java
+++ app/src/main/java/com/example/administrator/musicstart/activity/MusicActivity.java
@@ -2,21 +2,30 @@ package com.example.administrator.musicstart.activity;
 
 import android.app.Activity;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.example.administrator.musicstart.R;
+import com.example.administrator.musicstart.fragment.PlayFragment;
 
-public class MusicActivity extends Activity implements NavigationView.OnNavigationItemSelectedListener {
+public class MusicActivity extends FragmentActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private  NavigationView mNavigationView;
     private ImageView mIvMenu;
     private DrawerLayout mDrawerLayout;
+    private FrameLayout mFLPlayBar;
+
+    private boolean isPlayFragmentShow = false;
+    private PlayFragment mPlayFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,14 +43,41 @@ public class MusicActivity extends Activity implements NavigationView.OnNavigati
                 mDrawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+        mFLPlayBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPlayingFragment();
+            }
+        });
+    }
+
+    //显示播放页面
+    private void showPlayingFragment() {
+
+       /* if (isPlayFragmentShow) {
+            return;
+        }
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        ft.setCustomAnimations(R.anim.fragment_slide_up, 0);
+        if (mPlayFragment == null) {
+            mPlayFragment = new PlayFragment();
+            ft.replace(android.R.id.content, mPlayFragment);
+        } else {
+            ft.show(mPlayFragment);
+        }
+        ft.commitAllowingStateLoss();*/
+        isPlayFragmentShow = true;
     }
 
     //控件初始化
     private void init() {
 
-         mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
-         mIvMenu = (ImageView) findViewById(R.id.iv_menu);
-         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_music);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mIvMenu = (ImageView) findViewById(R.id.iv_menu);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_music);
+        mFLPlayBar = (FrameLayout) findViewById(R.id.fl_play_bar);
 
     }
 
---
+++ app/src/main/java/com/example/administrator/musicstart/fragment/PlayFragment.java
@@ -0,0 +1,25 @@
+package com.example.administrator.musicstart.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.musicstart.R;
+
+/**
+ * Created by Administrator on 2017/8/1.
+ */
+
+public class PlayFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_play, container, false);
+    }
+
+
+}
